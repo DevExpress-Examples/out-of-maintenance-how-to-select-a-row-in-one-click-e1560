22dea39cc0f363c77a8fcf63c977d06c4ac243be
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to select a row in one click
+
+
+<p>The ASPxGridView supports the <a href="http://demos.devexpress.com/ASPxGridViewDemos/Selection/SelectByRowClick.aspx"><u>Selection via Row Clicks</u></a>. However, in some cases it is required to have the selection feature, but the selection checkbox might have an unusual placement in the grid (e.g. in some templates). The sample demonstrate how to implement the Selection feature using a stand-alone checkbox that is created in DataItem template.</p><p><strong>See Also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/E1559">How to use ASPxCheckBox in DataItemTemplate to emulate a selection</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,18 @@
+Imports Microsoft.VisualBasic
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxGridView
+Imports System
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub cbCheck_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim cb As ASPxCheckBox = TryCast(sender, ASPxCheckBox)
+
+		Dim container As GridViewDataItemTemplateContainer = TryCast(cb.NamingContainer, GridViewDataItemTemplateContainer)
+		cb.ClientInstanceName = String.Format("cbCheck{0}", container.VisibleIndex)
+		cb.Checked = grid.Selection.IsRowSelected(container.VisibleIndex)
+
+		' This line doesn't work because RowClick event is fired when the checkbox is going to be checked
+		' cb.ClientSideEvents.CheckedChanged = String.Format("function (s, e) {{ grid.SelectRowOnPage ({0}, s.GetChecked()); }}", container.VisibleIndex);
+	End Sub
+End Class
